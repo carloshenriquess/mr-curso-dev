aad7a7aab168d0c04dac08378b5aea2c7ba03a18
Carlos Henrique <carlos.henrique0307200@gmail.com>
2023-12-30 12:00:17 -0300

    change prefix to `mr`

--- apps/mr-curso-dev/src/app/app.component.ts
+++ apps/mr-curso-dev/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
-  selector: 'mr-curso-dev-root',
+  selector: 'mr-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
